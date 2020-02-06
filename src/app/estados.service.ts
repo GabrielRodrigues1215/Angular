2d4c8ace66f46698d1d47cfbe1c5f48ba3592f75
GabrielRodrigues1215 <gabriel.playmovi@gmail.com>
2020-02-06 14:36:06 -0300

    ..

--- src/app/estados.service.ts
+++ src/app/estados.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstadosService {
 
-  url= "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
   constructor(private http: HttpClient) { }
   
   getEstados():Observable<any[]>{
-    return this.http.get<any[]>(this.url)  
+    let url = environment.urlAPI+"localidades/estados"
+    return this.http.get<any[]>(url);
 
   }
 
